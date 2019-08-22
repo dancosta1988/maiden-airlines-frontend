891a0d6d332dd04d5977ee6e6296c03815ba7db7
Daniel Costa <daniel.costa@polarising.com>
2019-08-22 17:47:23 +0100

    Operator view
    Operator has access to flights, airports and airplanes for now

--- maiden-web-app/src/app/app.module.ts
+++ maiden-web-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AirplanesComponent } from './airplanes/airplanes.component';
 import { AirportsComponent } from './airports/airports.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FlightsComponent } from './flights/flights.component';
+import { OperatorViewComponent } from './operator-view/operator-view.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { FlightsComponent } from './flights/flights.component';
     AirplanesComponent,
     AirportsComponent,
     FlightsComponent,
+    OperatorViewComponent,
   ],
   imports: [
     BrowserModule,
