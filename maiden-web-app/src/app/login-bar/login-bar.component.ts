6dc137bd1a7c52fd9d73010116ab619f4c51fa93
Daniel Costa <daniel.costa@polarising.com>
2019-08-27 18:00:49 +0100

    Fixes to login bar

--- maiden-web-app/src/app/login-bar/login-bar.component.ts
+++ maiden-web-app/src/app/login-bar/login-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-bar',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-bar.component.css']
 })
 export class LoginBarComponent implements OnInit {
+  signupForm: FormGroup;
+  loginForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    console.log("initializing login bar...");
+    //create forms
+    this.signupForm = new FormGroup({
+      'signupFirstName' : new FormControl(null,Validators.required),
+      'signupLastName' : new FormControl(null, Validators.required),
+      'signupAddress' : new FormControl(null, Validators.required),
+      'signupVAT' : new FormControl(null, Validators.required),
+      'signupDOB' : new FormControl(null, Validators.required),
+      'signupGender' : new FormControl(null, Validators.required),
+      'signupContact' : new FormControl(null, Validators.required),
+      'signupContactType' : new FormControl(null, Validators.required),
+      'signupEmail' : new FormControl(null, Validators.required),
+      'signupPassword' : new FormControl(null, Validators.required),
+      'signupConfirmPassword' : new FormControl(null, Validators.required)
+    });
+
+    this.loginForm = new FormGroup({
+      'loginEmail' : new FormControl(null, Validators.required),
+      'loginPassword' : new FormControl(null, Validators.required)
+    });
+  }
+
+  onLogin(){
+
+  }
+
+  onSignup(){
+
   }
 
 }
