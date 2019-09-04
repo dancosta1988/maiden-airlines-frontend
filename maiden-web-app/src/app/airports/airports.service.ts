fd958d3374e06965f6a0c9be357ff5d44327ae28
Daniel Costa <daniel.costa@polarising.com>
2019-09-04 17:55:14 +0100

    Fixes on Airports

--- maiden-web-app/src/app/airports/airports.service.ts
+++ maiden-web-app/src/app/airports/airports.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Airport } from './airport.model';
-import { map } from 'rxjs/internal/operators/map';
 import { ConstantsService } from '../common/services/constants.service';
 
 @Injectable({providedIn: 'root'})
@@ -12,17 +11,17 @@ export class AirportsService {
     createAndStoreAirport(name: string, short_name: string, city: string, country: string){
         console.log("inserting: " + name + " " + city + " " + country);
         //const postData: Airport = new Airport(null, name, short_name, city, country);
-        return this.http.post(this.constants.webServicesUrl+'/AirportCreate?shortName='+short_name+'&country='+country+'&city='+city+'&name='+name,null)
+        return this.http.post(this.constants.webServicesUrl+'/Airports/AirportCreate?shortName='+short_name+'&country='+country+'&city='+city+'&name='+name,null)
     }
 
     updateAirport(id: number, name: string, short_name: string, city: string, country: string){
         //const postData: Airport = { id: id, name: name, shortName: short_name, city: city, country: country};
-        return this.http.post(this.constants.webServicesUrl+'/AirportUpdate?id='+id+'&shortName='+short_name+'&country='+country+'&city='+city+'&name='+name,null);
+        return this.http.post(this.constants.webServicesUrl+'/Airports/AirportUpdate?id='+id+'&shortName='+short_name+'&country='+country+'&city='+city+'&name='+name,null);
     }
 
     deleteAirport(id: number){
         //const postData: Airport = { id: id, name: null, shortName: null, city: null, country: null};
-        return this.http.post(this.constants.webServicesUrl +'/AirportDelete?id='+id,null);
+        return this.http.post(this.constants.webServicesUrl +'/Airports/AirportDelete?id='+id,null);
     }
 
     fetchAirports(){
