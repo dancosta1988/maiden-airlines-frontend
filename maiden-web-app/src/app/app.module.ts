04a076948c22fbfd6b6a614b68b4e9d9baf697b1
Daniel Costa <daniel.costa@polarising.com>
2019-09-04 17:54:34 +0100

    Routing and stuff

--- maiden-web-app/src/app/app.module.ts
+++ maiden-web-app/src/app/app.module.ts
@@ -12,6 +12,15 @@ import { ConstantsService } from './common/services/constants.service';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginBarComponent } from './login-bar/login-bar.component';
+import { BackofficeRolesComponent } from './backoffice-roles/backoffice-roles.component';
+import { DatePipe } from '@angular/common';
+import { BackofficeOperatorsComponent } from './backoffice-operators/backoffice-operators.component';
+import { ClientTypesComponent } from './client-types/client-types.component';
+import { ClientsComponent } from './clients/clients.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { BookingComponent } from './booking/booking.component';
+import { BookingTypesComponent } from './booking-types/booking-types.component';
 
 @NgModule({
   declarations: [
@@ -23,15 +32,23 @@ import { LoginBarComponent } from './login-bar/login-bar.component';
     OperatorViewComponent,
     FooterComponent,
     HeaderComponent,
-    LoginBarComponent
+    LoginBarComponent,
+    BackofficeRolesComponent,
+    BackofficeOperatorsComponent,
+    ClientTypesComponent,
+    ClientsComponent,
+    HomeComponent,
+    BookingComponent,
+    BookingTypesComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
-  providers: [ConstantsService],
+  providers: [ConstantsService,DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
