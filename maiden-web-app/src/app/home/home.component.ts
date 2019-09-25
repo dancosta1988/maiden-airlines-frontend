8d7b7f2d797dd9530f63f3ef1b135d279aec4b53
Daniel Costa <daniel.costa@polarising.com>
2019-09-25 17:48:54 +0100

    Dynamic Flights on Home

--- maiden-web-app/src/app/home/home.component.ts
+++ maiden-web-app/src/app/home/home.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ConstantsService } from '../common/services/constants.service';
+import { HttpClient } from '@angular/common/http';
+import { FlightsService } from '../flights/flights.service';
+import { AirportsService } from '../airports/airports.service';
+import { Flight } from '../flights/flight.model';
+import { Airport } from '../airports/airport.model';
+import { Observable, timer } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +14,107 @@ import { ConstantsService } from '../common/services/constants.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private constants: ConstantsService) { }
+  public isFetching = false;
+  private fetchedAirplanes = false;
+  private fetchedAirports = false;
+  public error = "";
+  public success = "";
+
+  
+  public airports: Airport[] = [];
+  public flights: Flight[] = [];
+  public showflights: Flight[] = [];
+  public backgrounds: string[] = ["url('assets/geneva.jpg')", "url('assets/paris.jpg')", "url('assets/london.jpg')"];
+
+  everySecond: Observable<number> = timer(5000, 10000);
+  private subscription = this.everySecond.subscribe((seconds) => {
+      this.getRandomFlights();
+  })
+
+  constructor( private http: HttpClient, private flightsService: FlightsService, private airportsService: AirportsService, private constants: ConstantsService) { }
 
   ngOnInit() {
+
+    this.onRefresh();
+    
   }
+
+  getBackground(): Object{
+    return { 'background': this.backgrounds[Math.ceil(Math.random() * (this.backgrounds.length-1))], 'background-size': 'cover'};
+  }
+
+  getRandomFlights(){
+    this.showflights = [];
+    for(let i = 0; i < 3 && this.flights.length > 0; i++){
+      this.showflights.push(this.flights[Math.ceil(Math.random() * (this.flights.length-1))]);
+    }
+      
+  }
+
+  onRefresh(){
+    this.fetchAirports();
+    this.fetchFlights();
+  }
+
+     
+  onFetchFlights(){
+    this.fetchFlights();
+  }
+
+  private fetchFlights(){
+    this.isFetching = true;
+    
+      this.flightsService.fetchFlights().subscribe(flights =>{
+        this.isFetching = false;
+        this.flights = [];
+        for (var i = 0, len = flights.length; i < len; i++) {
+          this.flights.push(new Flight(flights[i].id, flights[i].flightNumber, flights[i].departureDate, this.getAirportById(flights[i].departureAirport), flights[i].arrivalDate, this.getAirportById(flights[i].arrivalAirport), null, flights[i].gate, flights[i].status, flights[i].price, flights[i].miles));
+        }
+        
+        this.getRandomFlights();
+        this.success ="";
+        this.error ="";
+      },
+      error =>{
+          this.isFetching = false;
+          this.success ="";
+          this.error = error.message;
+      });
+  }
+
+  onErrorClose(){
+    this.error = null;
+  }
+
+  onSuccessClose(){
+    this.success = null;
+  }
+
+
+  private fetchAirports(){
+    this.fetchedAirports = false;
+    this.airportsService.fetchAirports().subscribe(data =>{
+        
+        this.airports = [];
+        for (var i = 0, len = data.length; i < len; i++) {
+          this.airports.push(new Airport(data[i].id, data[i].name, data[i].shortName, data[i].city, data[i].country));
+        }
+        this.fetchedAirports = true;
+      },
+      error =>{
+          this.error = error.message;
+      });
+  }
+
+  public getAirportIndex(airport: Airport){
+    return this.airports.indexOf(airport);
+  }
+
+    public getAirportById(id:number){
+    let airport: Airport = this.airports.find(x => x.id === id);
+    return airport;
+  }
+
+   
 
 }
